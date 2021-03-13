2e3db06879803a67504eed14e6c24a07df6e9faa
German Ovalle Silva <govalle3@outlook.com>
2021-03-12 22:46:28 -0500

    Cambio de nombre del objeto

--- src/com/ceiba/AppTesting.java
+++ src/com/ceiba/AppTesting.java
@@ -9,7 +9,7 @@ public class AppTesting {
 
     @Test
     public void ProbarPromedio(){
-        Alumno alumno = new Alumno("LA123","Carlos",4.00,3.8);
+        Alumno alumno = new Alumno("LA123","Juan alberto",4.00,3.8);
         Double result;
         result = alumno.CalcularPromedio();
         assertEquals((4+3.8)/2,result,0);
