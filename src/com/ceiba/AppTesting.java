5a663384d57988dddea65ff5ee7e10c1f779d021
German Ovalle Silva <govalle3@outlook.com>
2021-03-12 22:54:03 -0500

    Nuevos nombres subida dos

--- src/com/ceiba/AppTesting.java
+++ src/com/ceiba/AppTesting.java
@@ -9,10 +9,10 @@ public class AppTesting {
 
     @Test
     public void ProbarPromedio(){
-        Alumno alumno = new Alumno("LA123","Juan alberto",4.00,3.8);
+        Alumno alumno = new Alumno("LA123","alberto",5.00,3.8);
         Double result;
         result = alumno.CalcularPromedio();
-        assertEquals((4+3.8)/2,result,0);
+        assertEquals((5+3.8)/2,result,0);
     }
 
 }
